d3bc1ccd41d9e8dfe18f1f8f8d168be6e30560a9
ho dev <hodev.dev@gmail.com>
2021-01-11 19:56:23 +0330

    integration to sanctum

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Counter } from './conponents/Counter';
+import { Axios } from './helper/axios_config';
+
 function App() {
+
+  useEffect(() => {
+    Axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
+      console.log({ response });
+    });
+  }, []);
+
   return (
     <Counter />
   );
---
+++ src/helper/axios_config.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+
+const refreshAuthLogic = () => axios.get('/sanctum/csrf-cookie').then((response) => Promise.resolve());
+
+createAuthRefreshInterceptor(axios, refreshAuthLogic, { statusCodes: [419, 401] });
+
+export { axios as Axios };
