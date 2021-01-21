8f353b505d1408de1536c4bdef609d1e387e5ff2
ho dev <hodev.dev@gmail.com>
2021-01-21 23:05:56 +0330

    soem bug fixes

--- src/conponents/Scafold.jsx
+++ src/conponents/Scafold.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scafold = (props) => {
   return (
-    <div className={"flex flex-col w-full h-full bg-arc-dark_2" + ' ' + props.className}>
+    <div className={"flex flex-col w-full h-screen bg-arc-dark_2" + ' ' + props.className}>
       {props.children}
     </div>
   )
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import Cover from '../assets/img/cover.jpg';
+import Header from '../conponents/Header';
+import Scafold from '../conponents/Scafold';
 import { Axios } from '../helper/axios_config';
 import { logout } from '../redux/authSlice';
 const User = () => {
-
   const dispatch = useDispatch();
   useEffect(() => {
     Axios.get('/api/user').then((response) => {
@@ -13,12 +15,18 @@ const User = () => {
         dispatch(logout());
       }
     });
-  }, [])
+  }, [dispatch])
 
   return (
-    <div>
-      user
-    </div>
+    <Scafold className={'items-center'}>
+      <Header />
+      <div className={'flex flex-row w-8/12 h-auto mt-5 rounded-lg bg-arc-dark_1'}>
+        <div className={'w-10/12'}>left</div>
+        <div className={'w-2/12 p-2'}>
+          <img className={"rounded-lg"} src={Cover} alt="" srcset="" />
+        </div>
+      </div>
+    </Scafold>
   )
 }
 
