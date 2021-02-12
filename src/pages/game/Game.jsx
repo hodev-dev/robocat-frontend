bdcefa18e780b0094cff1d45c0324071bfbc646e
ho dev <hodev.dev@gmail.com>
2021-02-12 16:13:01 +0330

    slider added

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -1,17 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'react-circular-progressbar/dist/styles.css';
 import { FiArrowUp, FiEye, FiFlag, FiMessageCircle } from "react-icons/fi";
 import { useDispatch, useSelector } from 'react-redux';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import TextareaAutosize from 'react-textarea-autosize';
 import Cover1 from '../../assets/img/cover.jpg';
+import Cover2 from '../../assets/img/cover2.jpg';
 import Header from '../../conponents/Header';
 import Scafold from '../../conponents/Scafold';
 import TabBar from "../../conponents/TabBar";
 import { findIdByName, gameTabSelector, select, tabs } from '../../redux/gameTabSlice';
+var Carousel = require('react-responsive-carousel').Carousel;
+
+
 
 const Game = () => {
   const dispatch = useDispatch();
   const _isMounted = useRef(true);
   const gameTabState = useSelector(gameTabSelector);
+  const [_tag1, _setTag1] = useState({});
+  const [_tag2, _setTag2] = useState({});
+  const [_tag3, _setTag3] = useState({});
+
 
   useEffect(() => {
     const tab = findIdByName(tabs, "Posts");
@@ -20,8 +30,29 @@ const Game = () => {
       _isMounted.current = false;
     }
   }, [])
+
+  const CustomDot = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      index,
+      active,
+      carouselState: { currentSlide, deviceType }
+    } = rest;
+    // const carouselItems = [CarouselItem1, CaourselItem2, CarouselItem3];
+    // onMove means if dragging or swiping in progress.
+    // active is provided by this lib for checking if the item is active or not.
+    return (
+      <button
+        className={active ? "active text-6xl" : "inactive text-6xl text-gray-500"}
+        onClick={() => onClick()}
+      >
+        .
+      </button>
+    );
+  };
+
   return (
-    <Scafold className={'items-center'}>
+    <Scafold className={'items-center '}>
       <Header />
       <div className={'flex flex-row w-full mt-0.5 h-auto p-1 rounded-lg bg-arc-dark_1 opacity-90'}>
         <div className={'flex items-center justify-center w-1/12 h-auto p-1 rounded-xl'}>
@@ -41,23 +72,46 @@ const Game = () => {
       </div>
       <div className={"flex justify-center w-full h-auto"}>
         <div className={"w-6/12 h-auto p-1 mt-4 bg-arc-dark_1 rounded-xl"}>
-          <input placeholder={"title"} className={"w-full p-4 text-lg text-white outline-none h-14 bg-arc-dark_1"} />
-          <input placeholder={"title"} className={"w-full p-4 text-lg text-white border-t-2 outline-none h-28 bg-arc-dark_1 border-arc-dark_2"} />
-          <div className={"flex items-center w-full ml-1 border-t-2 h-14 border-arc-dark_2"}>
-            <div className={"flex items-center justify-center w-24 h-10 text-xl font-bold text-white rounded-2xl bg-arc-accent_1"}>Post</div>
+          <input dir={"rtl"} placeholder={"title"} className={"w-full p-4 text-lg text-white outline-none h-14 bg-arc-dark_1"} />
+          <TextareaAutosize dir={"rtl"} placeholder={"write something"} className={"w-full p-4 text-lg text-white border-t-2 border-b-2 outline-none h-28 bg-arc-dark_1 border-arc-dark_2"} />
+          <div className={"flex flex-row flex-1"} dir={"ltr"}>
+            <input placeholder={"tags1"} className={"w-full h-12 p-4 text-lg text-center text-white border-r-2 outline-none bg-arc-dark_1 border-arc-dark_2"} />
+            <input placeholder={"tags2"} className={"w-full h-12 p-4 text-lg text-center text-white border-r-2 outline-none bg-arc-dark_1 border-arc-dark_2"} />
+            <input placeholder={"tags3"} className={"w-full h-12 p-4 text-lg text-center text-white border-r-2 outline-none bg-arc-dark_1 border-arc-dark_2"} />
+          </div>
+          <div className={"flex flex-row items-center w-full ml-1 border-t-2 h-14 border-arc-dark_2 font-shabnam"}>
+            <div className={"flex flex-row items-center justify-center w-24 h-10 ml-0.5 text-xl text-white bg-arc-accent_1 rounded-2xl"}>
+              <span className={"ml-1 font-semibold text-gray-200 "}>ارسال</span>
+            </div>
           </div>
         </div>
       </div>
       <div className={"flex items-center justify-center w-full h-auto"}>
         <div className={"w-6/12 h-auto mt-4 bg-arc-dark_1 "}>
-          <div className={"flex items-center w-full h-auto p-1 border-b-2 border-arc-dark_2"}>
-            <div className={"w-2 h-2 p-2 ml-2 text-xl text-white bg-yellow-200 rounded-full"}></div>
-            <h1 className={"p-2 text-xl text-white"}> Title</h1>
+          <div className={"flex items-center w-full h-auto p-1 border-b-2 border-arc-dark_2"} dir={"rtl"}>
+            <div className={"w-2 h-2 p-2 ml-2 mr-2 text-xl text-white bg-yellow-200 rounded-full"}></div>
+            <h1 className={"p-2 text-xl text-white"} > ویتچر</h1>
           </div>
-          <div className={"flex flex-col items-center justify-center w-full h-auto "}>
-            <img className={"object-cover w-full border border-arc-dark_2 "} src={Cover1} alt="" srcset="" />
+          <div className={"flex flex-row"}>
+            <Carousel showArrows={true} swipeable={true} emulateTouch={true} showThumbs={false}>
+              <div className={"flex flex-col items-center justify-center w-full h-auto select-none"}>
+                <img className={"object-cover w-full border border-arc-dark_2 "} src={Cover1} alt="" srcset="" />
+              </div>
+              <div className={"flex flex-col items-center justify-center w-full h-auto "}>
+                <img className={"object-cover w-full border border-arc-dark_2 "} src={Cover2} alt="" srcset="" />
+              </div>
+              <div className={"flex flex-col items-center justify-center w-full h-auto "}>
+                <img className={"object-cover w-full border border-arc-dark_2 "} src={Cover2} alt="" srcset="" />
+              </div>
+              <div className={"flex flex-col items-center justify-center w-full h-auto "}>
+                <img className={"object-cover w-full border border-arc-dark_2 "} src={Cover2} alt="" srcset="" />
+              </div>
+              <div className={"flex flex-col items-center justify-center w-full h-auto "}>
+                <img className={"object-cover w-full border border-arc-dark_2 "} src={Cover2} alt="" srcset="" />
+              </div>
+            </Carousel>
           </div>
-          <div className={"flex items-center w-full h-auto p-1"}>
+          <div className={"flex items-center w-full h-auto p-1 "}>
             <h1 className={"p-4 text-3xl text-white font-shabnam"} dir={"rtl"}>
               بازی «ویچر ۳: وایلد هانت» (Witcher3: Wild Hunt) چندهفته‌ای است که فانتزی‌دوستان نقش‌آفرینی‌باز را از کار و زندگی انداخته است. «ویچر ۳» که نقطه پایانی بر سفر گرالت ریویایی است،‌ حسابی از سوی منتقدان مورد ستایش قرار گرفته است. در ادامه با زومجی همراه باشید تا به بررسی این بازی  بپردازیم.
             </h1>
@@ -86,7 +140,7 @@ const Game = () => {
           </div>
         </div>
       </div>
-    </Scafold>
+    </Scafold >
   )
 }
 
